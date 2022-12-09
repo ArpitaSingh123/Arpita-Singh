0a734c7761d3e095fc885f5b4838ec5ab202ac80
ArpitaSingh123 <arpitasinghbst111@gmail.com>
2022-12-09 21:50:24 +0530

    Add files via upload
    In this project, a python program has to be developed to generate a random story every time user runs the program.
    It consists of different types of stories, moral stories, horror stories, fantasy stories and adventurous stories.

---
+++ pyca.py
@@ -0,0 +1,325 @@
+#horror stories
+
+
+
+
+
+
+a='''                                                      They got the definition wrong
+
+
+It has been said that the definition of insanity is "doing the same thing over and over and expecting different results". I understand the sentiment behind the saying, 
+but it's wrong.I entered the building on a bet. I was strapped for cash and didn't buy into the old legends of the hotel to begin with, so fifty bucks was more than 
+enough to get me do it. It was simple. Just reach the top floor, the 45th floor, shine my flashlight from a window.The hotel was old and broken, including the elevator,so
+ that meant hiking up the stairs. So up the stairs I went. As I reached each platform, I noted the old brass plaques displaying the floor numbers. 15, 16, 17, 18. I felt
+a little tired as I crept higher, but so far, no ghosts, no cannibals, no demons. Piece of cake.I can't tell you how happy I was as I entered that last stretch of 
+numbers. I joyfully counted them aloud at each platform. 40, 41, 42, 43, 44, 44. I stopped and looked back down the stairs. I must have miscounted, so I continued up.
+44. One more flight. 44. And then down ten flights. 44. Fifteen flights. 44.And so it's been for as long as I can remember. So really, insanity isn't doing something
+repeatedly and expecting different results. It's knowing that the results will never ever change; that each door leads to the same staircase, to the same number. 
+It's realizing you no longer fall asleep. It's not knowing whether you've been running for days or weeks or years. It's when the sobbing slowly turns into laughter.'''
+
+
+
+b='''                                                                    Timekeeper
+
+He had been given the watch on his tenth birthday. It was an ordinary grey plastic wristwatch in every respect except for the fact that it was counting down. “
+That is all of the time you have left in the world, son. Use it wisely.” And indeed he did. As the watch ticked away, the boy, now a man, lived life to the fullest. He 
+climbed mountains and swam oceans. He talked and laughed and lived and loved. The man was never afraid, for he knew exactly how much time he had left.Eventually, the 
+watch began its final countdown. The old man stood looking over everything he had done, everything he had built.He shook hands with his old business partner, the man
+who had long been his friend and confidant. His dog came and licked his hand, earning a pat on the head for its companionship. He hugged his son, knowing that he 
+had been a good father.  He kissed his wife on the forehead one last time.The old man smiled and closed his eyes.
+
+Then, nothing happened. The watch beeped once and turned off. The man stood standing there, very much alive. You would think that in that moment he would have been 
+overjoyed. Instead, for the first time in his life, the man was scared.'''
+
+
+
+
+
+
+
+
+
+c='''                                                                       There's no Reason to be Afraid
+
+When my sister Betsy and I were kids, our family lived for awhile in a charming old farmhouse. We loved exploring its dusty corners and climbing the apple tree in the
+backyard. But our favorite thing was the ghost.We called her Mother, because she seemed so kind and nurturing. Some mornings Betsy and I would wake up, and on each of 
+our nightstands, we'd find a cup that hadn't been there the night before. Mother had left them there, worried that we'd get thirsty during the night. She just wanted to
+take care of us.Among the house's original furnishings was an antique wooden chair, which we kept against the back wall of the living room. Whenever we were preoccupied,
+watching TV or playing a game, Mother would inch that chair forward, across the room, toward us. Sometimes she'd manage to move it all the way to the center of the room.
+We always felt sad putting it back against the wall. Mother just wanted to be near us.Years later, long after we'd moved out, I found an old newspaper article about the
+farmhouse's original occupant, a widow. She'd murdered her two children by giving them each a cup of poisoned milk before bed. Then she'd hanged herself.The article 
+included a photo of the farmhouse's living room, with a woman's body hanging from a beam. Beneath her, knocked over, was that old wooden chair, placed exactly in the 
+center of the room.'''
+
+
+
+#moral stories
+
+
+
+
+p='''                                                                       The Proud Rose
+
+Once upon a time, in a desert far away, there was a rose who was so proud of her beautiful looks. Her only complaint was growing next to an ugly cactus.
+Every day, the beautiful rose would insult and mock the cactus on his looks, all while the cactus remained quiet. All the other plants nearby tried to make the rose see 
+sense, but she was too swayed by her own looks.
+One scorching summer, the desert became dry, and there was no water left for the plants. The rose quickly began to wilt. Her beautiful petals dried up, losing their lush
+color.
+Looking to the cactus, she saw a sparrow dip his beak into the cactus to drink some water. Though ashamed, the rose asked the cactus if she could have some water. The 
+kind cactus readily agreed, helping them both through the tough summer, as friends.
+
+Moral of the story: Never judge anyone by the way they look.'''
+
+
+
+
+
+
+q='''                                                                         The Farmer and the well
+
+
+One day, a farmer was looking for a water source for his farm, when he bought a well from his neighbor. The neighbor, however, was cunning. The next day, as the farmer
+ came to draw water from his well, the neighbor refused to let him take any water. When the farmer asked why, the neighbor replied, “I sold you the well, not the water,”
+ and walked away. Distraught, the farmer went to the emperor to ask for justice. He explained what had happened.
+The emperor called on Birbal, one of his nine, and wisest, courtiers. Birbal proceeded to question the neighbor, “Why don't you let the farmer take water from the well? 
+You did sell the well to the farmer?”
+The neighbor replied, “Birbal, I did sell the well to the farmer but not the water within it. He has no right to draw water from the well.”
+Birbal said, “Look, since you sold the well, you have no right to keep the water in the farmer's well. Either you pay rent to the farmer, or take it out immediately.” 
+Realizing that his scheme had failed, the neighbor apologized and went home.
+
+Moral of the story: Cheating will not get you anything. If you cheat, you'll pay soon enough.'''
+
+
+
+r='''                                                                   Elephant and friends
+
+
+
+A lone elephant walked through the forest, looking for friends. She soon saw a monkey and proceeded to ask, 'Can we be friends, monkey?' The monkey quickly replied, 'You
+ are big and can't swing on trees like I do, so I cannot be your friend.' Defeated, the elephant continued to search when it stumbled across a rabbit. She proceeded to 
+ ask him, 'Can we be friends, rabbit?' The rabbit looked at the elephant and replied, “You are too big to fit inside my burrow. You cannot be my friend.”
+Then, the elephant continued until she met a frog. She asked, “Will you be my friend, frog?”
+The frog replied, “You are too big and heavy; you cannot jump like me. I am sorry, but you can't be my friend.”
+The elephant continued to ask the animals she met on her way, but always received the same reply. The following day, the elephant saw all the forest animals run in fear.
+ She stopped a bear to ask what was happening and was told the tiger was attacking all the small animals.
+The elephant wanted to save the other animals, so she went to the tiger and said, “Please,sir, leave my friends alone. Do not eat them.”
+The tiger didn't listen. He merely told the elephant to mind her own business. Seeing no other way, the elephant kicked the tiger and scared him away. Upon hearing of 
+the brave tale, the other animals agreed, “You are just the right size to be our friend.”
+
+Moral of the story: Friends come in every shape and size. '''
+
+
+
+#thriller stories
+
+
+s='''                                                           Blue monkeys
+
+
+Cautiously,the girl stepped out of her cosy bed because she could hear tapping on the window which gave her fright. She silently crept over to the window and the icy, 
+cold, wooden floorboards creaked behind her. With lots of courage she slowly opened the curtains but to her surprise, she couldn't see anything. She looked again into the dark 
+night to check but she was sur that nothing was there. All of a sudden, the girl could see too bright lights drawing closer and closer out of the darkness. The girl was 
+shocked! What could they be?'''
+
+
+
+t='''                                                   Splodgy bananas says
+
+Silently, she crwaled out of the bed. There it was again, tap,tap,tap. She shivered to the window out of fright. She drew the curtains open, but all that was there were 
+the stars of the night sky. Suddenly, two bright yellow lights were coming closer and closer.
+Then a face appeared. It had sharp teeth but she realised that it was only her bestfriend Jack. She begged him to come in. Jack dashed in . She got a closer look at his 
+fangs and realised he was the vampire. He was tempted to bite Emily. What would he do?'''
+
+
+
+
+v='''                                                                Spooky spy girls!
+
+
+Breathlessly panting, the girls raced towards the dark,spooky forest as the wind was blowing ferociously. The ground was as hard as rock and the tree branches crouched 
+over their heads like a terrifying monster. Suddenly, a rustle of crunchy, red leaves started to a blow madly.
+Without a warning, a shadow figure slyly came out of the bushes and blocked Lizzie's way. The shadow figure had an axe  and cut a tree down. CRAK, CRACK, CRACK! The 
+humunguous tree started falling. Lizzie started to call her friend but she carried on running into the cold, darkness of the forest.'''
+
+
+
+
+
+#Adventure stories
+
+a1='''                                                  A SCOTTISH FACE IN THE DARK
+
+
+You don't realize how unfit you are till you've participated in a night scavenger hunt with a Scottish mountaineering club. My flatmate talked Nathan and I into it, so 
+off we went,equipped with flashlight aka "torch", and a friend who could navigate. As I huffed and puffed up the heathery hills, I couldn't help but wonder what I'd 
+gotten myself into. We walked across mountain tops with nothing but sheep and waded through muck and mire.
+At the end, a roaring boonfire greeted us , and everyone toasted a job well-done, even the mostly american team which casme inm the last.
+
+
+
+'''
+b1='''                                                  ALMOST A PRAGUE PRISON 
+
+
+Don't ride the teams in prague without a ticket . Sounds like common sense, but my friend and I thought one quick stop would'mt hurt .
+Oh, we were noticed .
+Two men asked for our tickets . Having none , we were escorted off the tram at the next stop . But we weren't set free . The men demanded we pasy the hefty fine for 
+riding without a ticket .They wouldn't listen to our pleas and marched to us on ATM . They said "either you pay or we go to czech police."90 dollars poorer . I'll never 
+again try to ride  a tram for free.
+
+
+
+'''
+c1='''                                                              THE DAYTONA 500 
+
+
+
+Daytona Beach , 2004 . My dance team was invited to be part of the famous NASCAR race: in a patriotic opening number. We were only a small number in a massive crowd. I 
+had to aarive at 3 am and practice for two straight days in the rain, but there were a few standoutsmoments during the event.
+President George W. Bush waved (at me, I swear) as he exited his helicopter. Right before performing her big number "ROCK in the USA", I saw LeeAnn Rimes mouth the words
+,"oh shit". All in all, a good day to be on live TV!'''
+
+
+
+#fantasy stories
+a2='''                                                      Alarm Clocks
+
+
+I always set my alarm clock before I go to sleep and I always set the alarm clock on my phone as a backup. Even though I set two alarms every morning, I wake up before 
+time. But I still set those alarms, just in case I sleep late and need them. It's like circus acrobats and trapeze artists who use nets. If they are good, they don't 
+need or even want the net, but they have it there anyway.
+So, I set the alarms, go to bed, and I dream of being a circus acrobat without a goddamned net'''
+
+
+
+b2='''                                                              Up in the air
+
+The circus came to town the day I left. I took Lisa to the show, knowing she still loved the clowns. First act was juggling .Lisa watched the balls arced and swooped, 
+then exploded into the air like multicolored popcorn. Their movement was a series of juggling and playful acts in which she was getting lost as the time was passing.
+She was enjoying each and every moment and the most she liked was charming flying elephants and monkeys. One monkey came near to her and played with her the game of
+cards. 
+She was so charmed with it that after two weeks also she is behaving like she is one of the jokers there.'''
+
+
+
+
+c3='''                                                       Camelot's Backyard
+
+
+We were only warriors. Eight-years-olds forging new frontiers, watching Rome fall. We were brothers in arms,bandits, thieves- but you named me damsel. We shaped palace
+walls from snow, tore through forbidden forest with plastic machetes.   We ate poisened berries like our stomachs were made of Excalibur, melted down and recast. We
+could never die. You, with your cardboard helmet and shield, were noblest of all. You gutted more dragons than the rest of us combined. But I, with my cheap silk cape and
+tissue paper crown, was stronger. I was never warrior, never damsel. I was made to rule.'''
+
+
+
+
+
+
+
+import random
+horror_list=[a,b,c]
+thriller_list=[s,t,v]
+moral_list=[p,q,r]
+adventure_list=[a1,b1,c1]
+fantasy_list=[a2,b2,c3]
+
+print("1. Thriller Stories")
+print("2. Horror Stories")
+print("3. Moral Stories")
+print("4. Adventure Stories")
+print("5. Fantasy Stories")
+print()
+
+while True:
+    a=int(input("                        Select the genre you like:    "))
+    if 1<=a<=5:
+        break
+    else:
+        print("Invalid")
+        continue
+
+
+if a==1:
+    print(random.choice(thriller_list))
+elif a==2:
+    print(random.choice(horror_list))
+elif a==3:
+    print(random.choice(moral_list))
+elif a==4:
+    print(random.choice(adventure_list))
+else:
+    print(random.choice(fantasy_list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
